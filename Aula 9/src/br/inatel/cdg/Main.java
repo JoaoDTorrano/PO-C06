6883e107c115688b40ae1a82d08343911cb3dc06
João Pedro Torrano Dias <130389414+JoaoDTorrano@users.noreply.github.com>
2025-09-02 12:27:41 -0300

    Add files via upload

---
+++ Aula 9/src/br/inatel/cdg/Main.java
@@ -0,0 +1,12 @@
+package br.inatel.cdg;
+
+import br.inatel.cdg.matematica.Matematica;
+
+public class Main {
+    public static void main(String[] args) {
+        //jeito dificil kkkkkk
+        //br.inatel.cdg.matematica.Matematica m = new br.inatel.cdg.matematica.Matematica();
+
+        Matematica m = new Matematica();
+    }
+}
